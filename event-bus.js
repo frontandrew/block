50242a80a72edb09d5c1e41dd23de75d7a2bc32c
AndrewBubnov <jackblack.nt@gmail.com>
2024-02-28 13:50:22 +0500

    chore: format init files

--- event-bus.js
+++ event-bus.js
@@ -1,34 +1,34 @@
 class EventBus {
     constructor() {
-      this.listeners = {};
+        this.listeners = {};
     }
   
     on(event, callback) {
-      if (!this.listeners[event]) {
-        this.listeners[event] = [];
-      }
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
   
-      this.listeners[event].push(callback);
+        this.listeners[event].push(callback);
     }
   
     off(event, callback) {
-          if (!this.listeners[event]) {
-        throw new Error(`Нет события: ${event}`);
-      }
-  
-      this.listeners[event] = this.listeners[event].filter(
-        listener => listener !== callback
-      );
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+    
+        this.listeners[event] = this.listeners[event].filter(
+            listener => listener !== callback
+        );
     }
   
-      emit(event, ...args) {
-      if (!this.listeners[event]) {
-        throw new Error(`Нет события: ${event}`);
-      }
-      
-      this.listeners[event].forEach(function(listener) {
-        listener(...args);
-      });
+    emit(event, ...args) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+        }
+        
+        this.listeners[event].forEach(function(listener) {
+            listener(...args);
+        });
     }
-  }
+}
   
