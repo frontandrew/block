a467e7be26dd80252918b20a45b62c7dd16ed4e2
AndrewBubnov <jackblack.nt@gmail.com>
2024-03-13 12:56:47 +0500

    feat: attach browser events

--- src/shared/ui/core/block/index.js
+++ src/shared/ui/core/block/index.js
@@ -20,11 +20,12 @@ export class Block {
         const eventBus = new EventBus();
         this.count = 0; // temporary
 
-        const { props, children } = this._separatePropsWithCildren(args);
+        const { props, children, events } = this._separatePropsWithCildren(args);
         
-        this._meta = { props, children };
+        this._meta = { props, children, events };
         this.children = this._makePropsProxy(children);
         this.props = this._makePropsProxy(props);
+        this.events = this._makePropsProxy(events);
         this.eventBus = () => eventBus;
         this._registerEvents(eventBus);        
         eventBus.emit(Block.EVENTS.INIT);
@@ -33,13 +34,15 @@ export class Block {
     _separatePropsWithCildren(args) {
         const props = {};
         const children = {};
+        const events = {};
 
         Object.entries(args).forEach(function([key, value]) {
             if (value instanceof Block) children[key] = value;
+            if (key.startsWith('on')) events[key] = value;
             else props[key] = value;
         });
 
-        return { props, children };
+        return { props, children, events };
     }
   
     _registerEvents(eventBus) {
@@ -53,7 +56,7 @@ export class Block {
         console.log(`INIT[${this.id}]`);
         this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
         const children = Object.values(this.children);
-        console.log('Before children render', children)
+        // console.log('Before children render', children)
         if (children.length > 0) children.forEach((child) => this._element.append(child.element));
     }
     
@@ -128,7 +131,8 @@ export class Block {
         }        
 
         this._element = resultElement;
-        console.log(`RNDR[${this.id}]::${++this.count}`, this.props)
+        this._attachEvents(this.events);
+        console.log(`RNDR[${this._element?.nodeName + '::' + this.id}]::${++this.count}`, { ...this._meta, elem: this._element })
     }
     
     // Может переопределять пользователь, необязательно трогать
@@ -167,6 +171,16 @@ export class Block {
         });
 
         return proxyProps;
+    }
+
+    _attachEvents() {
+        // const { events = {} } = this.props;
+        if (Object.keys(this.events).length <= 0) return;
+
+        for (const [key, value] of Object.entries(this.events)) {
+            console.log(`EVENT[${this._element?.nodeName + '::' + this.id}]::${this.count}`, { ...this._meta, elem: this._element })
+            this._element.addEventListener(key.toLowerCase().split(2), value)
+        }
     }
     
     show() {
